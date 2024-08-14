a047d295448809e4e5c483dd9e9eb49a485ebd0b
Kevin <kevin@brytonsport.com>
2024-08-14 16:49:49 +0800

    remove demo

--- nbdev_hello_world/_modidx.py
+++ nbdev_hello_world/_modidx.py
@@ -5,5 +5,5 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://kevin194.github.io',
                 'git_url': 'https://github.com/kevin194/nbdev-hello-world',
                 'lib_path': 'nbdev_hello_world'},
-  'syms': { 'nbdev_hello_world.core': {'nbdev_hello_world.core.foo': ('core.html#foo', 'nbdev_hello_world/core.py')},
+  'syms': { 'nbdev_hello_world.core': {'nbdev_hello_world.core.bar': ('core.html#bar', 'nbdev_hello_world/core.py')},
             'nbdev_hello_world.demo': {'nbdev_hello_world.demo.baz': ('demo.html#baz', 'nbdev_hello_world/demo.py')}}}
